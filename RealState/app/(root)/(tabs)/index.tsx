91043fa84daa2b9bca0a27645472c6364741f689
AnuragKharwar <anurag.singh.tmk@gmail.com>
2025-01-29 17:20:52 +0530

    udpate

---
+++ RealState/app/(root)/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+import { Link } from "expo-router";
+
+export default function Page() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-extrabold text-gray-900">
+        Hello Anurag
+      </Text>
+
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile </Link>
+    </View>
+  );
+}
---
+++ RealState/app/_layout.tsx
@@ -0,0 +1,26 @@
+import { Slot, SplashScreen, Stack } from "expo-router";
+import "../global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+export default function RootLayout() {
+  const [fontLoaded] = useFonts({
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+  if (!fontLoaded) {
+    return null;
+  }
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
