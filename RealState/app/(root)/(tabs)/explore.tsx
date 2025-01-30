01e4e60e48094201031bea8a3c0785116f0a417b
AnuragKharwar <anurag.singh.tmk@gmail.com>
2025-01-30 21:40:08 +0530

    added explroe section and property detail page

--- RealState/app/(root)/(tabs)/explore.tsx
+++ RealState/app/(root)/(tabs)/explore.tsx
@@ -1,11 +1,88 @@
-import React from "react";
-import { Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+
+import icons from "@/constants/icons";
+import { Card } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import NoResults from "@/components/NoResults";
+import { dummyProperties } from "@/constants/data";
 
 const Explore = () => {
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+  const [filteredProperties, setFilteredProperties] = useState(dummyProperties);
+  const [loading, setLoading] = useState(false);
+  const query = params.filter;
+
+  useEffect(() => {
+    if (query && query == "All") {
+      setFilteredProperties(dummyProperties);
+    } else if (query) {
+      setFilteredProperties(
+        dummyProperties.filter((item) => item.category.includes(query))
+      );
+    }
+  }, [query]);
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
   return (
-    <View>
-      <Text> Explore </Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <FlatList
+        data={filteredProperties}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5 w-full "
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+          ) : (
+            <NoResults />
+          )
+        }
+        ListHeaderComponent={() => (
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <TouchableOpacity
+                onPress={() => router.back()}
+                className="flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center"
+              >
+                <Image source={icons.backArrow} className="size-5" />
+              </TouchableOpacity>
+
+              <Text className="text-base mr-2 text-center font-rubik-medium text-black-300">
+                Search for Your Ideal Home
+              </Text>
+              <Image source={icons.bell} className="w-6 h-6" />
+            </View>
+
+            {/* <Search /> */}
+
+            <View className="mt-5">
+              <Filters />
+
+              <Text className="text-xl font-rubik-bold text-black-300 mt-5">
+                Found {filteredProperties.length} Properties
+              </Text>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
